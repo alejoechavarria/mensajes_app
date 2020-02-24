0185d69764946f6c76c2f52df0482b6677384580
Alejo Echavarria <alejandroechavarria96@gmail.com>
2020-02-24 00:05:03 -0500

    Inicio de proyecto

---
+++ src/main/java/com/examples/mensajes_app/Conexion.java
@@ -0,0 +1,21 @@
+package com.examples.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_connection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app", "root", "");
+            if (connection!=null){
+                System.out.println("Conexion Exitosa");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return  connection;
+    }
+}
